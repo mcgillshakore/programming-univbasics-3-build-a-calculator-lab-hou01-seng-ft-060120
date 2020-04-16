26a638276fe2ab5ce6dc83c0a68ad20330479392
Shakore McGill <mcgills2093@gmail.com>
2020-04-16 20:48:43 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+sum = num2 + num1
+sum
 end
 
 def subtraction(num1, num2)
-
+sub = num1 - num2
+sub
 end
 
 def division(num1, num2)
-
+divide = num1 / num2
+divide
 end
 
 def multiplication(num1, num2)
-
+  multiple = num1 * num2
+  multiple
 end
 
 def modulo(num1, num2)
-
+  percent = num1 % num2
+  percent
 end
 
 def square_root(num)
-
+  sum = Math.sqrt(num)
+  sum
 end
